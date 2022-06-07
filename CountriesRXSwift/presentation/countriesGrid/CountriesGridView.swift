72bb044502d37ec60c8dc40113d0b70c5e8fcc71
Mwai Banda <bandamwai@gmail.com>
2022-06-06 21:56:36 -0500

    SwiftUI Interpolation final phase Added: CountryViewDetail

--- CountriesRXSwift/presentation/countriesGrid/CountriesGridView.swift
+++ CountriesRXSwift/presentation/countriesGrid/CountriesGridView.swift
@@ -17,17 +17,40 @@ struct CountryGridView: View {
     var body: some View {
         ZStack {
             Color(.systemGray5).ignoresSafeArea(.all)
-            ScrollView {
-                ForEach(countryGridViewModel.countries.chunks(ofCount: Int(screenBounds.width/95)), id: \.self) { row in
+            ScrollView(showsIndicators: false) {
+                VStack(spacing: 0)  {
+                ForEach(
+                    countryGridViewModel.countries
+                    .chunks(ofCount: Int(screenBounds.width/140)),
+                    id: \.self
+                ) { row in
+                    VStack(spacing: 0)  {
+                    Divider()
+
                     HStack(alignment: .top) {
+                        Spacer()
                         ForEach(row, id: \.id) { country in
+
                             NavigationLink {
+                                CountryDetailView(country: country)
                             } label: {
                                 CountryView(country: country)
+                                    .padding(.vertical, 15)
+                                    .frame(maxWidth: 130)
                             }
+                            
+                            if country.id != row.last?.id {
+                            Divider()
+                                    .padding(.horizontal)
+                            }
+                            
                         }
-                    }.padding(.horizontal)
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    }
                 }
+            }
             }
             .padding(.top, 1)
             .frame(width: screenBounds.width)
@@ -48,12 +71,17 @@ struct CountryView: View {
             WebImage(url: URL(string: country.flagURL ?? ""), context: [.imageThumbnailPixelSize : CGSize.zero])
                 .resizable()
                 .placeholder(Image(systemName: "photo"))
+                .indicator(Indicator.progress)
+                .transition(.fade(duration: 0.5))
+                .aspectRatio(contentMode: .fill)
                 .foregroundColor(.gray.opacity(0.5))
-                .frame(width: 95, height: 75)
+                .frame(width: 100, height: 75)
                 .cornerRadius(7)
             
             Text(country.name)
-                .fontWeight(.semibold)
+                .font(.caption)
+                .fontWeight(.bold)
+                .foregroundColor(.black)
                 .multilineTextAlignment(.center)
         }
     }
--- CountriesRXSwift/presentation/countriesGrid/CountryDetailView.swift
+++ CountriesRXSwift/presentation/countriesGrid/CountryDetailView.swift
@@ -6,16 +6,52 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct CountryDetailView: View {
     var country: Country
     var body: some View {
-        Text(country.name)
+        ZStack {
+            Color(.systemGray5).ignoresSafeArea(.all)
+
+        ScrollView {
+            VStack(alignment: .center) {
+                HStack {
+                    Text(country.name)
+                        .font(.title)
+                        .fontWeight(.black)
+                        .padding(.leading)
+                    Spacer()
+                }.padding(.leading)
+                
+                WebImage(url: URL(string: country.flagURL ?? ""), context: [.imageThumbnailPixelSize : CGSize.zero])
+                    .resizable()
+                    .placeholder(Image(systemName: "photo"))
+                    .indicator(Indicator.progress)
+                    .transition(.fade(duration: 0.5))
+                    .foregroundColor(.gray.opacity(0.5))
+                    .frame(width: screenBounds.width - 90, height: 211)
+                
+                VStack {
+                    ForEach(country.cities ?? ["No Cities Available"], id: \.self) { city in
+                        HStack {
+                            Text(city)
+                            Spacer()
+                        }.padding(.vertical, 7)
+                            .padding(.horizontal)
+                        Divider()
+                            .padding(.horizontal)
+                    }
+                } .padding(.horizontal)
+                Spacer()
+            }
+        }.offset(y: -35)
+        }
     }
 }
 
 struct CountryDetailView_Previews: PreviewProvider {
     static var previews: some View {
-        CountryDetailView(country: Country(name: "Zambia", unicodeFlag: "🇿🇲"))
+        CountryDetailView(country: Country(name: "Zambia", unicodeFlag: "🇿🇲", cities: ["Ndola", "Lusaka"]))
     }
 }
